d64d75cacfe32fe5966a85779bbb1dc7fd10eef1
unknown <shehzad>
2025-11-07 15:31:41 +0500

    Integrate Routing

--- resources/views/layouts/auth/footer.blade.php
+++ resources/views/layouts/auth/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
             <div class="text-body">
                 Copyright &copy;
-                {{ date('Y') }} developed by <a href="{{ route('welcome') }}" class="text-muted">Inaam ul haq</a>
+                {{ date('Y') }} developed by <a href="{{ route('welcome') }}" class="text-muted">M Shehzad</a>
             </div>
             <div class="d-none d-lg-inline-block">
             </div>
