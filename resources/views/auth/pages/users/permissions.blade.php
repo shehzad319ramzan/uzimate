3937d91a41991eaeb7bd6425f4d62b56a443dd23
unknown <shehzad>
2025-11-21 12:34:44 +0500

    dynamic dashboard

--- resources/views/auth/pages/users/permissions.blade.php
+++ resources/views/auth/pages/users/permissions.blade.php
@@ -14,21 +14,30 @@
 
                     <div class="row">
                         @foreach ($permissions as $category => $items)
-                            <div class="col-md-4 mb-4">
-                                <div class="border rounded p-3 h-100">
-                                    <h6 class="fw-semibold mb-3">{{ $category }}</h6>
-                                    @foreach ($items as $permission)
-                                        <div class="form-check mb-2">
-                                            <input class="form-check-input" type="checkbox" name="permissions[]"
-                                                id="perm_{{ $permission->name }}" value="{{ $permission->name }}"
-                                                {{ in_array($permission->name, $assigned) ? 'checked' : '' }}>
-                                            <label class="form-check-label" for="perm_{{ $permission->name }}">
-                                                {{ $permission->title ?? Str::title(str_replace('_', ' ', $permission->name)) }}
-                                            </label>
-                                        </div>
-                                    @endforeach
+                            @php
+                                // Restricted categories that only superadmin can see
+                                $restrictedCategories = ['Permissions', 'Roles', 'Website Settings'];
+                                $isRestrictedCategory = in_array($category, $restrictedCategories);
+                                $isSuperAdmin = auth()->user()->hasRole('super_admin');
+                            @endphp
+                            
+                            @if (!$isRestrictedCategory || $isSuperAdmin)
+                                <div class="col-md-4 mb-4">
+                                    <div class="border rounded p-3 h-100">
+                                        <h6 class="fw-semibold mb-3">{{ $category }}</h6>
+                                        @foreach ($items as $permission)
+                                            <div class="form-check mb-2">
+                                                <input class="form-check-input" type="checkbox" name="permissions[]"
+                                                    id="perm_{{ $permission->name }}" value="{{ $permission->name }}"
+                                                    {{ in_array($permission->name, $assigned) ? 'checked' : '' }}>
+                                                <label class="form-check-label" for="perm_{{ $permission->name }}">
+                                                    {{ $permission->title ?? Str::title(str_replace('_', ' ', $permission->name)) }}
+                                                </label>
+                                            </div>
+                                        @endforeach
+                                    </div>
                                 </div>
-                            </div>
+                            @endif
                         @endforeach
                     </div>
 
